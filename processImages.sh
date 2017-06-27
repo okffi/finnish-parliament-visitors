2b0cd42ff9676e008fc1792b607bc0d94afeed56
Aleksi Knuutila <aleksi.knuutila@iki.fi>
2017-06-27 16:34:38 +0300

    page_id creation in processImages.sh

--- processImages.sh
+++ processImages.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
- 
+
 # This script runs through all folders that have the following structure
 #
 #           BuildingRegister#date#HadBeenExpected
 
 # BuildingRegister : There are two separate registers for different buildings
 # (g or b)          Blue folder is for the temporary use of the Sibelius Academy -> b
-#                   Green folder for the parliament itself  -> g            
+#                   Green folder for the parliament itself  -> g
 
 # Date            : Date of the page register. This is hard to retrieve from individual pages
 # (YYYY-MM-DD)     and therefore has to be inputed in the folder as an extra metadata.
@@ -16,16 +16,25 @@
 #
 
 
-# Exports: A folder called 'processed_photos' in each of the above where the 
-# images found are color balanced and rotated if they are portraits. 
+# Exports: A folder called 'processed_photos' in each of the above where the
+# images found are color balanced and rotated if they are portraits.
 
-# *** The rotation is not guaranteed since it is but default clockwise ** 
-# *** But the files have a check_ tag so it is enough to look at them to see if they 
+# *** The rotation is not guaranteed since it is but default clockwise **
+# *** But the files have a check_ tag so it is enough to look at them to see if they
 # *** are readable.
 
 # Run with ./cleanLobbyPhotos and input the folder to process
 # errors appear with '_' in front of the erronous folder names
-  
+
+# Set this to be different for every run of photographs. This will affect the IDs assigned to pages.
+# By setting a different base for IDs, there are no collisions between the IDs.
+    PHOTOGRAPH_DATE='2706'
+
+    function get_page_id {
+      alphanum=`echo "$1" | tr 0123456789 abcdefghij`
+      echo "$PHOTOGRAPH_DATE$alphanum"
+    }
+
     echo Which folder \do you want to process?
     read parentFolder
     cd $parentFolder
@@ -43,13 +52,13 @@
         cd $d
 
         #get the name of the current directory which contains metadata on the information
-        currentDir=${PWD##*/} 
+        currentDir=${PWD##*/}
         echo "Trying to process folder: " $currentDir
 
         IFS='#' read -r -a params <<< "$currentDir"
 
         BuildingRegister=${params[0]} # folder g or b
-        Date=${params[1]} # date YYYY-MM-DD 
+        Date=${params[1]} # date YYYY-MM-DD
         HadBeenExpected=${params[2]}  # ne or e expected or not
 
         # echo $BuildingRegister
@@ -66,10 +75,10 @@
         elif [[  -z "$BuildingRegister" ]]; then
             echo $currentDir' is missing BuildingRegister param .. skipping..'
             mv "$parentFolder/$d" "$parentFolder/_$d"
-        else 
+        else
             mkdir -p processed_photos
 
-            for img in *.jpg ; do
+            for img in *.jpg *.JPG ; do
                 identify=$(identify "$img")
                 [[ $identify =~ ([0-9]+)x([0-9]+) ]] || \
                     { echo Cannot get size >&2 ; continue ; }
@@ -77,6 +86,7 @@
                 height=${BASH_REMATCH[2]}
 
                 # Generate new name for the image file
+                fn_page_id=`get_page_id $page_id`
                 rr="${Date}#${BuildingRegister}#${HadBeenExpected}#$dir_id-$page_id.jpg"
                 echo $rr
                 page_id=$((page_id+1))
@@ -93,8 +103,8 @@
                 if (( width < height )) ; then
                     echo "$img is portrait, turning it clockwise but check it "
                      rr_rot="check_$rr"
-                     convert "$parentFolder/$d/processed_photos/$rr" -rotate 270 "$parentFolder/$d/processed_photos/$rr"  
-                     
+                     convert "$parentFolder/$d/processed_photos/$rr" -rotate 270 "$parentFolder/$d/processed_photos/$rr"
+
                      mv -f -i "$parentFolder/$d/processed_photos/$rr" "$parentFolder/$d/processed_photos/$rr_rot"
 
                 fi
